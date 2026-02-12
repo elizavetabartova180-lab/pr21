04e327f0ff394fef1f9f201da2c7b9f3f7f6bf86
ADMIN <ADMIN@MSI>
2026-02-13 01:18:06 +0500

    Реализованные модели. Bartova

---
+++ Model/Document.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents_Bartova.Model
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Scr { get; set; }
+        public string Name { get; set; }
+        public string User { get; set; }
+        public int IdDocument { get; set; }
+        public DateTime Date { get; set; }
+        public int Status { get; set; }
+        public int Description { get; set; }
+    }
+}
