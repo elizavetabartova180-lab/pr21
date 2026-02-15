f6d952eaefcd484ad26b173442ffa8e73f690636
PC <PC@FEDEMER-N5095A>
2026-02-16 00:09:16 +0500

    Добавьте файлы проекта.

---
+++ Classes/Common/DBConnection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents_Bartova.Classes.Common
+{
+    public class DBConnection
+    {
+        public static readonly string Path = @"C:\Users\PC\Downloads\pr21-master\pr21-master\bin\Debug\DataBase.accbd";
+        public static OleDbConnection Connection()
+        {
+            OleDbConnection connection = new OleDbConnection(Path);
+            connection.Open();
+            return connection;
+        }
+        public static OleDbDataReader Querty (string sql, OleDbConnection connection)
+        {
+            OleDbCommand command = new OleDbCommand(sql, connection);
+            return command.ExecuteReader();
+        }
+        public static void CloseConnection(OleDbConnection connection) { 
+            connection.Close();
+        }
+    }
+}
---
+++ Interfaces/IDocument.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Documents_Bartova.Interfaces
+{
+    public interface IDocument
+    {
+        void Save(bool update = false);
+        List<Model.Document> AllDocuments();
+        void Delete();
+    }
+}
