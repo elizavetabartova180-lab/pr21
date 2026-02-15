c227db7722c3b8147519d86829a79a696a4c4ba5
ADMIN <ADMIN@MSI>
2026-02-15 23:50:45 +0500

    промежуточный

---
+++ Classes/Common/DBConnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents_Bartova.Classes.Common
+{
+    internal class DBConnection
+    {
+    }
+}
